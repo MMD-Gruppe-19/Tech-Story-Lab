9528653dbcc7f8e7e9001866da37f4119019700b
Laura Mølnitz <lamo0004@stud.kea.dk>
2024-09-10 15:19:18 +0200

    fetch

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,24 @@
 const url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products";
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bXppaXZ6a3RrcXdkaWZiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTk2NjQsImV4cCI6MjA0MTUzNTY2NH0.jnqs_SZqG2ry9d4WiNown_yXJAmzVQ_K_FX8kFI1zr8";
 
+// fetch(url)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok ' + response.statusText);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
 
 fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+  .then((svar) => svar.json())
+  .then((data) => visProdukt(data));
+
+function visProdukt(data) {
+  console.log(data);
+}
