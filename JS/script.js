befabf26617d99ba364e4c5c5fc7719ef728f088
Massimo <max1000.dede1000@gmail.com>
2024-09-10 16:13:43 +0200

    Jeg har fixet det så det burde fungere nu. Hvis I tjekker på consollen, fungerer det.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
-const url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products";
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bXppaXZ6a3RrcXdkaWZiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTk2NjQsImV4cCI6MjA0MTUzNTY2NH0.jnqs_SZqG2ry9d4WiNown_yXJAmzVQ_K_FX8kFI1zr8";
+// const url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products";
+// const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bXppaXZ6a3RrcXdkaWZiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTk2NjQsImV4cCI6MjA0MTUzNTY2NH0.jnqs_SZqG2ry9d4WiNown_yXJAmzVQ_K_FX8kFI1zr8";
 
 // fetch(url)
 //   .then(response => {
@@ -15,10 +15,34 @@ const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZi
 //     console.error('There was a problem with the fetch operation:', error);
 //   });
 
-fetch(url)
-  .then((svar) => svar.json())
-  .then((data) => visProdukt(data));
+// fetch(url)
+//   .then((svar) => svar.json())
+//   .then((data) => visProdukt(data));
 
-function visProdukt(data) {
-  console.log(data);
-}
+// function visProdukt(data) {
+//   console.log(data);
+// }
+
+const url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bXppaXZ6a3RrcXdkaWZiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTk2NjQsImV4cCI6MjA0MTUzNTY2NH0.jnqs_SZqG2ry9d4WiNown_yXJAmzVQ_K_FX8kFI1zr8";
+
+fetch(url, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'apikey': key,
+    'Authorization': `Bearer ${key}`
+  }
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
