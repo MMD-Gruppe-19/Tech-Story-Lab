524630a0ec52c3e7ec499f616c18ecc6effb9ad6
Laura Mølnitz <lamo0004@stud.kea.dk>
2024-09-11 12:06:13 +0200

    Omskrevet

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,14 @@
-// const url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products";
-// const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bXppaXZ6a3RrcXdkaWZiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTk2NjQsImV4cCI6MjA0MTUzNTY2NH0.jnqs_SZqG2ry9d4WiNown_yXJAmzVQ_K_FX8kFI1zr8";
+const url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bXppaXZ6a3RrcXdkaWZiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTk2NjQsImV4cCI6MjA0MTUzNTY2NH0.jnqs_SZqG2ry9d4WiNown_yXJAmzVQ_K_FX8kFI1zr8";
 
-// fetch(url)
+// fetch(url, {
+//   method: 'GET',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'apikey': key,
+//     'Authorization': `Bearer ${key}`
+//   }
+// })
 //   .then(response => {
 //     if (!response.ok) {
 //       throw new Error('Network response was not ok ' + response.statusText);
@@ -15,34 +22,16 @@
 //     console.error('There was a problem with the fetch operation:', error);
 //   });
 
-// fetch(url)
-//   .then((svar) => svar.json())
-//   .then((data) => visProdukt(data));
-
-// function visProdukt(data) {
-//   console.log(data);
-// }
-
-const url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products";
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bXppaXZ6a3RrcXdkaWZiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTk2NjQsImV4cCI6MjA0MTUzNTY2NH0.jnqs_SZqG2ry9d4WiNown_yXJAmzVQ_K_FX8kFI1zr8";
-
+// Jeg har omskrevet det ovenstående til det vi har lært i undervisningen i stedet
 fetch(url, {
-  method: 'GET',
+  method: "GET", //Denne er default GET
   headers: {
-    'Content-Type': 'application/json',
-    'apikey': key,
-    'Authorization': `Bearer ${key}`
-  }
+    apikey: key, //Vi sender vores key med
+  },
 })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+  .then((res) => res.json())
+  .then(showData);
+
+function showData(data) {
+  console.log(data);
+}
