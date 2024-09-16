44ce2d6497c4a8fb04b4b1086e098812f90ec0a8
Laura Mølnitz <lamo0004@stud.kea.dk>
2024-09-16 14:15:02 +0200

    java

--- JS/script.js
+++ JS/script.js
@@ -35,3 +35,18 @@ fetch(url, {
 function showData(data) {
   console.log(data);
 }
+
+// Jeg har leget lidt med at udtrække unikke objektkoder, så vi kan få et overblik over de forskellige produkter
+fetch("https://symziivzktkqwdifbaap.supabase.co/rest/v1/products?select=Objektkode", {
+  method: "GET",
+  headers: {
+    apikey: key,
+  },
+})
+  .then((response) => response.json()) // Konverter responsen til JSON
+  .then((data) => {
+    // Udtræk unikke objektkoder
+    const objektkoder = [...new Set(data.map((item) => item.Objektkode))];
+    // Udskriv de unikke objektkoder
+    console.log(objektkoder);
+  });
