ae70676b17ffc2265fbec339eb86e38e518b6a50
matilde <mabr0005@stud.kea.dk>
2024-09-17 15:28:11 +0200

    henter kategori fra supabase

--- JS/listeview.js
+++ JS/listeview.js
@@ -1,29 +1,26 @@
 const urlParams = new URLSearchParams(window.location.search);
-let url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products?" + kategori;
+const kategori = (urlParams.get("kategori"));
+let url = "https://symziivzktkqwdifbaap.supabase.co/rest/v1/products?kategori=eq." + kategori;
+
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bXppaXZ6a3RrcXdkaWZiYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTk2NjQsImV4cCI6MjA0MTUzNTY2NH0.jnqs_SZqG2ry9d4WiNown_yXJAmzVQ_K_FX8kFI1zr8";
-const category = (urlParams.get("kategori"));
 
 const skabelon = document.querySelector("template").content;
 const container = document.querySelector(".products");
 
-/* window.addEventListener("load", hentData); */
 
-fetch(url, {
-    method: "GET", 
-    headers: {
-      apikey: key, 
-    },
-  })
+window.addEventListener("load", hentData);
+
+function hentData(){
+    fetch(url, {
+        method: "GET", 
+        headers: {
+            apikey: key, 
+        },
+    })
     .then((res) => res.json())
-    .then(showData);
+    .then(visProdukter);
+}
 
-    fetch(url)
-    .then((response) => response.json())
-    .then((categories) => {
-      categories.forEach((category) => {
-        categoryList.innerHTML += `<li><a href="listeview.html?category=${category.category}">${category.category}</a></li>`;
-      });
-    });
 
 /* function hentData() {
     fetch(url)
