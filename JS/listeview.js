afa8c600a7ca5b450d606b91e7babe1ed9b86626
matilde <mabr0005@stud.kea.dk>
2024-09-18 14:51:49 +0200

    js flyttet fra list til nav

--- JS/listeview.js
+++ JS/listeview.js
@@ -36,19 +36,27 @@ function visProdukter(produkter) {
     });
 };
 
-// navigations javascript
+/* // navigations javascript
 function toggleMenu() {
     const navbar = document.getElementById('navbar');
     navbar.classList.toggle('hidden');
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     // Find alle links i navigationen
     const navLinks = document.querySelectorAll("#navbar ul ul a");
 
-    // Opdater href for hvert link
+    // Find den aktuelle URL
+    const currentUrl = window.location.href;
+
+    // Opdater href for hvert link og tilføj 'active' klasse til den aktuelle
     navLinks.forEach(link => {
-      const category = link.textContent.trim(); // Hent tekstindhold og fjern evt. whitespace
-      link.href = `listeview.html?kategori=${encodeURIComponent(category)}`;
+        const category = link.textContent.trim(); // Hent tekstindhold og fjern evt. whitespace
+        link.href = `listeview.html?kategori=${encodeURIComponent(category)}`;
+
+        // Tilføj 'active' klasse hvis linkets href matcher den aktuelle URL
+        if (link.href === currentUrl) {
+            link.classList.add("active");
+        }
     });
-  });
+}); */
