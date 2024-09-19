f583e26d26e08adcaa56fd045b6578fbe44966df
matilde <mabr0005@stud.kea.dk>
2024-09-19 08:52:48 +0200

    sfgjsd

--- JS/navbar.js
+++ JS/navbar.js
@@ -2,22 +2,3 @@ function toggleMenu() {
     const navbar = document.getElementById('navbar');
     navbar.classList.toggle('hidden');
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Find alle links i navigationen
-    const navLinks = document.querySelectorAll("#navbar ul ul a");
-
-    // Find den aktuelle URL
-    const currentUrl = window.location.href;
-
-    // Opdater href for hvert link og tilføj 'active' klasse til den aktuelle
-    navLinks.forEach(link => {
-        const category = link.textContent.trim(); // Hent tekstindhold og fjern evt. whitespace
-        link.href = `listeview.html?kategori=${encodeURIComponent(category)}`;
-
-        // Tilføj 'active' klasse hvis linkets href matcher den aktuelle URL
-        if (link.href === currentUrl) {
-            link.classList.add("active");
-        }
-    });
-});
