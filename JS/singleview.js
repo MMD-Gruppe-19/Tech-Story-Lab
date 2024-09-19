eb284aaa8ed736c507f0ed9c568f3bce3ad28516
matilde <mabr0005@stud.kea.dk>
2024-09-19 11:32:33 +0200

    Arbejde med listview

--- JS/singleview.js
+++ JS/singleview.js
@@ -4,15 +4,15 @@ console.log(urlParams);
 const url = `https://symziivzktkqwdifbaap.supabase.co/rest/v1/products/${Objektkode}`;
 
 function getProduct() {
-    fetch(url)
+  fetch(url)
     .then((res) => res.json())
     .then(visProdukt);
 }
 
 function visProdukt(produkt) {
-document.querySelector("h2").textContent = produkt.produktnavn;
-document.querySelector(".brand").textContent = produkt.brand;
-document.querySelector(".objektkode").textContent = produkt.Objektkode;
+  document.querySelector("h2").textContent = produkt.produktnavn;
+  document.querySelector(".brand").textContent = produkt.brand;
+  document.querySelector(".objektkode").textContent = produkt.Objektkode;
 }
 
 getProduct();
