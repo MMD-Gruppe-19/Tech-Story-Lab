d9768d86d8c9cb360f4e0f8baebf193b7a8d0759
matilde <mabr0005@stud.kea.dk>
2024-09-19 10:54:11 +0200

    single view

--- JS/listeview.js
+++ JS/listeview.js
@@ -27,8 +27,8 @@ function visProdukter(produkter) {
     document.querySelector("h2").textContent = kategori; //Viser den kategori man er inde på 
     produkter.forEach((produkt) => {
         console.log(produkt);
+        // her kloner jeg min template, så jeg kan få vist flere produkter i stedet for bare det ét 
         const kopi = skabelon.cloneNode(true);
-        //kopi.querySelector("img").src = ``;
         kopi.querySelector("h3").textContent = produkt.produktnavn;
         kopi.querySelector("p").textContent = produkt.brand;
         kopi.querySelector(".read-more").href = `singleview.html?id=${produkt.Objektkode}`;
@@ -36,27 +36,3 @@ function visProdukter(produkter) {
     });
 };
 
-/* // navigations javascript
-function toggleMenu() {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('hidden');
-}
-
-  document.addEventListener("DOMContentLoaded", function() {
-    // Find alle links i navigationen
-    const navLinks = document.querySelectorAll("#navbar ul ul a");
-
-    // Find den aktuelle URL
-    const currentUrl = window.location.href;
-
-    // Opdater href for hvert link og tilføj 'active' klasse til den aktuelle
-    navLinks.forEach(link => {
-        const category = link.textContent.trim(); // Hent tekstindhold og fjern evt. whitespace
-        link.href = `listeview.html?kategori=${encodeURIComponent(category)}`;
-
-        // Tilføj 'active' klasse hvis linkets href matcher den aktuelle URL
-        if (link.href === currentUrl) {
-            link.classList.add("active");
-        }
-    });
-}); */
---
+++ JS/singleview.js
@@ -0,0 +1,18 @@
+const urlParams = new URLSearchParams(window.location.search);
+const Objektkode = urlParams.get("Objektkode");
+console.log(urlParams);
+const url = `https://symziivzktkqwdifbaap.supabase.co/rest/v1/products/${Objektkode}`;
+
+function getProduct() {
+    fetch(url)
+    .then((res) => res.json())
+    .then(visProdukt);
+}
+
+function visProdukt(produkt) {
+document.querySelector("h2").textContent = produkt.produktnavn;
+document.querySelector(".brand").textContent = produkt.brand;
+document.querySelector(".objektkode").textContent = produkt.Objektkode;
+}
+
+getProduct();
